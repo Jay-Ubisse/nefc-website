03153137c195e3f0ba6731cc9bce1ff3aa2d371d
geniamaria <mariageniaabilio@gmail.com>
2023-10-28 22:12:19 +0200

    add partiners section and footer

--- src/components/en/Footer.tsx
+++ src/components/en/Footer.tsx
@@ -1,7 +1,46 @@
 export const Footer = () => {
-    return ( 
-        <div>
-            Footer Component
-        </div>
-     );
+    return (
+        <footer className=" p-10 bg-gradient-to-tl from-red-700 to-app-blue-200 text-white">
+            <div className="footer">
+                <nav>
+                    <header className="font-bold text-xl text-white">Services</header>
+                    <a className="link link-hover">Online Learning, Tutoring and Creation</a>
+                    <a className="link link-hover">General Consulting Service</a>
+                    <a className="link link-hover">Representation of International Partners</a>
+                    <a className="link link-hover">International Business Facilitation</a>
+                    <a className="link link-hover">Project Development and Management</a>
+                </nav>
+                <nav>
+                    <header className="font-bold text-xl text-white">Company</header>
+                    <a className="link link-hover">About us</a>
+                    <a className="link link-hover">Contact</a>
+                    <a className="link link-hover">Services</a>
+                    <a className="link link-hover">Projects</a>
+                </nav>
+                <nav>
+                    <header className="font-bold text-xl text-white">Legal</header>
+                    <a className="link link-hover">Terms of use</a>
+                    <a className="link link-hover">Privacy policy</a>
+                    <a className="link link-hover">Cookie policy</a>
+                </nav>
+                <form>
+                    <header className="font-bold text-xl text-white">Newsletter</header>
+                    <fieldset className="form-control w-80">
+                        <label className="label">
+                            <span className="label-text text-white">Enter your email address</span>
+                        </label>
+                        <div className="relative">
+                            <input type="text" placeholder="username@site.com" className="input input-bordered w-full pr-16 text-slate-900" />
+                            <button className="btn btn-primary bg-app-blue-200 border-none hover:bg-app-blue-200/80 absolute top-0 right-0 rounded-l-none">Subscribe</button>
+                        </div>
+                    </fieldset>
+                </form>
+            </div>
+            <div>
+                <p className="text-center text-sm mt-10">
+                    &copy; 2023 NEFC Projects Lda. Designed and Developed by <span className="font-bold underline underline-offset-1">Binario</span>.
+                </p>
+            </div>
+        </footer>
+    );
 }
--- src/pages/en/HomePage.tsx
+++ src/pages/en/HomePage.tsx
@@ -1,13 +1,21 @@
 import 'react-slideshow-image/dist/styles.css'
 import { Slide } from 'react-slideshow-image';
+import { Briefcase, ChalkboardTeacher, ChartLineUp, Globe, TreeStructure } from '@phosphor-icons/react';
+import { Fade, Slide as SlideAwesome } from 'react-awesome-reveal';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 import { Header } from "../../components/en/Header"
 import Image1 from "../../assets/img/services/service-1.jpg"
 import Image2 from "../../assets/img/services/service-2.jpg"
 import Image3 from "../../assets/img/services/service-3.jpg"
 import Image4 from "../../assets/img/services/service-4.jpg"
 import Image5 from "../../assets/img/services/service-5.jpg"
-import { Briefcase, ChalkboardTeacher, ChartLineUp, Globe, TreeStructure } from '@phosphor-icons/react';
-import { Fade, Slide as SlideAwesome } from 'react-awesome-reveal';
+import image7 from "../../assets/img/how-we-work/smart.webp"
+import image8 from "../../assets/img/how-we-work/smarter.webp"
+import image9 from "../../assets/img/how-we-work/fast.webp"
+import { Footer } from '../../components/en/Footer';
 
 const divStyle = {
   display: 'flex',
@@ -16,6 +24,15 @@ const divStyle = {
   backgroundSize: 'cover',
   height: '400px',
 }
+
+const settings = {
+  dots: true,
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 5000
+};
 
 const slideImages = [
   {
@@ -48,17 +65,17 @@ const slideImages = [
 export const HomePage = () => {
 
   return (
-    <div className="overflow-hidden">
+    <div className="overflow-x-hidden">
       <Header />
       <main className="w-screen">
-        <section className="bg-background1 bg-cover h-[calc(100vh+10rem)]">
-          <div className="bg-gradient-to-tr from-red-700/80 to-app-blue-200/80 h-full w-full">
+        <section className="bg-background1 bg-cover h-[calc(100vh-5rem)] overflow-visible">
+          <div className="bg-gradient-to-tr from-red-700/80 to-app-blue-200/80 h-full w-full overflow-visible">
             <SlideAwesome duration={2000}>
               <h1 className='font-normal text-3xl text-white w-1/3 mx-auto text-center pt-20'>
                 With NEFC PROJECTS LDA We Work Together For a Better Youth Generation
               </h1>
             </SlideAwesome>
-            <Fade duration={5000}>
+            <Fade duration={3000}>
               <div className="slide-container w-1/2 mx-auto mt-10">
                 <Slide>
                   {slideImages.map((slideImage, index) => (
@@ -78,7 +95,101 @@ export const HomePage = () => {
             </Fade>
           </div>
         </section>
+        <section className='w-screen pt-32 pb-16'>
+          <h1 className='text-4xl text-app-blue-200 font-semibold text-center'>
+            How We Work
+          </h1>
+          <hr className='h-2 w-10 bg-app-blue-200 mx-auto rounded-lg mt-2' />
+          <div className='flex shadow-lg shadow-red-700 w-2/3 mx-auto mt-10 h-60'>
+            <div className='w-2/5 flex'>
+              <img src={image7} alt="" className='h-40' />
+              <img src={image8} alt="" className='h-36 relative top-20 -left-16' />
+            </div>
+            <div className='w-3/5 pt-5 px-3'>
+              <h2 className='text-2xl text-app-blue-200 font-semibold mb-6'>
+                Smart
+              </h2>
+              <p className='text-slate-800'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse provident possimus perferendis in, vel et alias praesentium quaerat,
+                eius dignissimos facilis autem corporis, nesciunt vero vitae. Quisquam odit natus quas!
+              </p>
+            </div>
+          </div>
+          <div className='flex flex-row-reverse shadow-lg shadow-red-700 w-2/3 mx-auto mt-10 h-60'>
+            <div className='w-2/5 flex'>
+              <img src={image9} alt="" className='h-full ml-auto' />
+            </div>
+            <div className='w-3/5 pt-5 pl-20'>
+              <h2 className='text-2xl text-app-blue-200 font-semibold mb-6'>
+                Fast
+              </h2>
+              <p className='text-slate-800'>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse provident possimus perferendis in, vel et alias praesentium quaerat,
+                eius dignissimos facilis autem corporis, nesciunt vero vitae. Quisquam odit natus quas!
+              </p>
+            </div>
+          </div>
+        </section>
+        <section className='w-screen bg-app-blue-100 h-[calc(100vh-10rem)] p-1'>
+          <h1 className='text-4xl text-app-blue-200 font-semibold text-center mt-20'>
+            What We Do
+          </h1>
+          <hr className='h-2 w-10 bg-app-blue-200 mx-auto rounded-lg mt-2 border-none' />
+          <p className='mt-10 text-slate-900 text-center w-2/3 mx-auto'>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit reiciendis inventore id eaque voluptatem ad quibusdam,
+            tenetur assumenda non odio at, sequi vitae dolore. Minus quos quis perferendis quibusdam earum?
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit reiciendis inventore id eaque voluptatem ad quibusdam,
+            tenetur assumenda non odio at, sequi vitae dolore. Minus quos quis perferendis quibusdam earum?
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Impedit reiciendis inventore id eaque voluptatem ad quibusdam,
+            tenetur assumenda non odio at, sequi vitae dolore. Minus quos quis perferendis quibusdam earum?
+          </p>
+        </section>
+        <section className='w-screen bg-background2 bg-cover h-[calc(100vh-10rem)]'>
+          <div className='bg-gradient-to-r from-app-blue-200/80 to-red-700/50 w-full h-full text-white p-1'>
+            <Slider {...settings}>
+              <div>
+                <h3 className='text-xl text-white font-normal text-center mt-20'>
+                  The Business Time
+                </h3>
+                <hr className='h-[1px] w-20 bg-white  border-none mx-auto rounded-lg mt-2 mb-20' />
+                <h1 className='text-4xl font-semibold w-1/2 mx-auto mb-20 text-center'>
+                  The smartest services in the market
+                </h1>
+              </div>
+              <div>
+                <h3 className='text-xl text-white font-normal text-center mt-20'>
+                  Technology Today
+                </h3>
+                <hr className='h-[1px] w-20 bg-white  border-none mx-auto rounded-lg mt-2 mb-20' />
+                <h1 className='text-4xl font-semibold w-1/2 mx-auto mb-20 text-center'>
+                  The game changing project you'll need
+                </h1>
+              </div>
+            </Slider>
+          </div>
+        </section>
+        <section className='w-screen bg-white h-[calc(100vh-10rem)] p-1 mb-20'>
+          <h1 className='text-4xl text-app-blue-200 font-semibold text-center mt-20'>
+            Our Partners
+          </h1>
+          <hr className='h-2 w-10 bg-app-blue-200 mx-auto rounded-lg mt-2 border-none' />
+          <div className='flex justify-around mt-20'>
+            <div className='h-60 w-60 shadow-lg shadow-red-700 bg-white text-app-blue-200 text-xl font-semibold flex items-center justify-center'>
+              Partner 1
+            </div>
+            <div className='h-60 w-60 shadow-lg shadow-red-700 bg-white text-app-blue-200 text-xl font-semibold flex items-center justify-center'>
+              Partner 1
+            </div>
+            <div className='h-60 w-60 shadow-lg shadow-red-700 bg-white text-app-blue-200 text-xl font-semibold flex items-center justify-center'>
+              Partner 1
+            </div>
+            <div className='h-60 w-60 shadow-lg shadow-red-700 bg-white text-app-blue-200 text-xl font-semibold flex items-center justify-center'>
+              Partner 1
+            </div>
+          </div>
+        </section>
       </main>
+      <Footer />
     </div>
   )
 }
