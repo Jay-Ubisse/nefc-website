b511f0eba6adc448a84af70e40ff8e8287e00a18
Jay-Ubisse <joaquimubisse@gmail.com>
2023-10-30 13:38:39 +0200

    Update about page

--- src/pages/en/AboutPage.tsx
+++ src/pages/en/AboutPage.tsx
@@ -1,7 +1,7 @@
 import { Footer } from "../../components/en/Footer";
 import { Header } from "../../components/en/Header"
 import logo from "../../assets/img/logo.jpg"
-import { Eye } from "@phosphor-icons/react";
+import { CaretDoubleRight, Eye } from "@phosphor-icons/react";
 
 export const AboutPage = () => {
     return (
@@ -13,7 +13,7 @@ export const AboutPage = () => {
                         <div className='w-2/5 pt-20'>
                             <img src={logo} alt="" />
                         </div>
-                        <div className='w-3/5 pt-2 px-3 pl-5'>
+                        <div className='w-3/5 pt-2 px-3 pl-5 text-slate-800'>
                             <h2 className='text-4xl text-app-blue-200 font-semibold mb-2'>
                                 Who We Are
                             </h2>
@@ -37,7 +37,7 @@ export const AboutPage = () => {
                                 <Eye size={72} weight="bold" className="mx-auto" />
                                 <h3>Our Vision</h3>
                             </div>
-                            <p className="text-sm">
+                            <p className="text-sm text-slate-800">
                                 Our vision is to revolutionize business conceptualization through positive business
                                 solution and tailored edutech. As a trusted partner, we are committed to seamless
                                 global expansion, sustainable project development, and expert guidance. We also plan
@@ -45,21 +45,127 @@ export const AboutPage = () => {
                                 providing comprehensive IT tutoring and channeling youth entrepreneurship.
                             </p>
                         </div>
-                        <div className="text-center w-[45%]">
+                        <div className="text-center w-[45%] text-slate-800">
                             <div className="text-blue-800 mb-4">
                                 <Eye size={72} weight="bold" className="mx-auto" />
                                 <h3>Core Values</h3>
                             </div>
                             <p>
-                                At NEFC Projects Limitada, we driven toward empowering the 
-                                youth and maximizing their potential: 
+                                At NEFC Projects Limitada, we driven toward empowering the
+                                youth and maximizing their potential:
                             </p>
                             <ul className="text-left ml-14 list-disc">
-                                    <li>Excellence;</li>
-                                    <li>Integrity;</li>
-                                    <li>Collaboration;</li>
-                                    <li>Innovation.</li>
-                                </ul>
+                                <li>Excellence;</li>
+                                <li>Integrity;</li>
+                                <li>Collaboration;</li>
+                                <li>Innovation.</li>
+                            </ul>
+                        </div>
+                    </section>
+                    <section className='rounded-lg w-2/3 mx-auto p-10 mt-10 bg-white'>
+                        <h2 className='text-4xl text-app-blue-200 font-semibold mb-2'>
+                            Why Us
+                        </h2>
+                        <hr className='h-2 w-20 bg-red-700 rounded-lg mb-5 border-none' />
+                        <ul className="ml-16 mt-10 text-slate-800">
+                            <li className="flex gap-2">
+                                <span><CaretDoubleRight size={24} className="text-red-700" /></span>
+                                <span>
+                                    <strong>Expert Solutions: </strong>
+                                    Our experienced team and partners deliver comprehensive and insightful
+                                    solutions, fueling our client’s path to success.
+                                </span>
+                            </li>
+                            <li className="flex gap-2 my-2">
+                                <span><CaretDoubleRight size={24} className="text-red-700" /></span>
+                                <span>
+                                    <strong>Holistic services: </strong>
+                                    Enjoy the convenience of our all-encompassing range of services,
+                                    addressing diverse client needs under one roof.
+                                </span>
+                            </li>
+                            <li className="flex gap-2 my-2">
+                                <span><CaretDoubleRight size={24} className="text-red-700" /></span>
+                                <span>
+                                    <strong>Global Connections: </strong>
+                                    Access a robust global network that opens doors to expanded opportunities,
+                                    propelling your growth and businesses.
+                                </span>
+                            </li>
+                            <li className="flex gap-2">
+                                <span><CaretDoubleRight size={24} className="text-red-700" /></span>
+                                <span>
+                                    <strong>Youth Empowerment: </strong>
+                                    We are driven by a dedication to empowering the youth for a successful future,
+                                    shaping up tomorrow's leaders. With our extensive expertise, global network,
+                                    holistic approach, and unwavering dedication, youth will be positively empowered
+                                    through a profitable partnership that will enable them to achieve desired success.
+                                </span>
+                            </li>
+                        </ul>
+                    </section>
+                    <section className="w-[90%] mx-auto glass rounded-xl mt-10">
+                        <div className="w-full h-full bg-gradient-to-l rounded-xl from-app-blue-200/50 to-red-700/30 pt-10">
+                            <h2 className='text-4xl text-center text-white font-semibold mb-3'>
+                                Our Management Team
+                            </h2>
+                            <hr className='h-2 w-20 mx-auto bg-white rounded-lg mb-5 border-none' />
+                            <div className="grid grid-cols-3 justify-items-center gap-y-8 py-16">
+                                <div className="card w-80 glass">
+                                    <figure><img src={logo} alt="car!" /></figure>
+                                    <div className="card-body">
+                                        <h2 className="card-title text-white">Fred Nyembue</h2>
+                                        <p className="text-slate-50">
+                                            General Director
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="card w-80 glass">
+                                    <figure><img src={logo} alt="car!" /></figure>
+                                    <div className="card-body">
+                                        <h2 className="card-title text-white">Fred Nyembue</h2>
+                                        <p className="text-slate-50">
+                                            General Director
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="card w-80 glass">
+                                    <figure><img src={logo} alt="car!" /></figure>
+                                    <div className="card-body">
+                                        <h2 className="card-title text-white">Fred Nyembue</h2>
+                                        <p className="text-slate-50">
+                                            General Director
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="card w-80 glass">
+                                    <figure><img src={logo} alt="car!" /></figure>
+                                    <div className="card-body">
+                                        <h2 className="card-title text-white">Fred Nyembue</h2>
+                                        <p className="text-slate-50">
+                                            General Director
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="card w-80 glass">
+                                    <figure><img src={logo} alt="car!" /></figure>
+                                    <div className="card-body">
+                                        <h2 className="card-title text-white">Fred Nyembue</h2>
+                                        <p className="text-slate-50">
+                                            General Director
+                                        </p>
+                                    </div>
+                                </div>
+                                <div className="card w-80 glass">
+                                    <figure><img src={logo} alt="car!" /></figure>
+                                    <div className="card-body">
+                                        <h2 className="card-title text-white">Fred Nyembue</h2>
+                                        <p className="text-slate-50">
+                                            General Director
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </section>
                 </div>
